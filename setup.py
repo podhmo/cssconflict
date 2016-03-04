4549f4329dd13cee8de01a8a47ee5b58989aa9e8
podhmo <podhmo+altair@beproud.jp>
2016-03-04 12:16:57 +0900

    bump version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ testing_extras = tests_require + [
 ]
 
 setup(name='cssconflict',
-      version='0.1',
+      version='0.1.1',
       description='css coflict checking',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
